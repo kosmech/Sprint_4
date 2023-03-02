46a8a8df53672654be6589d0fb25b268c6037143
Konstantin Mechik <konstantinmechik@yandex.ru>
2023-03-02 18:23:12 +0300

    add text in README and test_questions.py

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -7,8 +7,8 @@ from locators.main_page_locators import *
 
 class TestQuestions:
 
-    @allure.title('Тест 1-го вопроса')
-    def test_text_questions_1(self, driver):
+    @allure.title('Тест 1-го вопроса. Проверка ответа')
+    def test_questions_1(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -17,8 +17,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_0) == 'Сутки — 400 рублей. Оплата курьеру — наличными или картой.'
 
-    @allure.title('Тест 2-го вопроса')
-    def test_text_questions_2(self, driver):
+    @allure.title('Тест 2-го вопроса. Проверка ответа')
+    def test_questions_2(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -27,8 +27,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_1) == 'Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.'
 
-    @allure.title('Тест 3-го вопроса')
-    def test_text_questions_3(self, driver):
+    @allure.title('Тест 3-го вопроса. Проверка ответа')
+    def test_questions_3(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -37,8 +37,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_2) == 'Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.'
 
-    @allure.title('Тест 4-го вопроса')
-    def test_text_questions_4(self, driver):
+    @allure.title('Тест 4-го вопроса. Проверка ответа')
+    def test_questions_4(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -47,8 +47,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_3) == 'Только начиная с завтрашнего дня. Но скоро станем расторопнее.'
 
-    @allure.title('Тест 5-го вопроса')
-    def test_text_questions_5(self, driver):
+    @allure.title('Тест 5-го вопроса. Проверка ответа')
+    def test_questions_5(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -57,8 +57,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_4) == 'Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.'
 
-    @allure.title('Тест 6-го вопроса')
-    def test_text_questions_6(self, driver):
+    @allure.title('Тест 6-го вопроса. Проверка ответа')
+    def test_questions_6(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -67,8 +67,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_5) == 'Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.'
 
-    @allure.title('Тест 7-го вопроса')
-    def test_text_questions_7(self, driver):
+    @allure.title('Тест 7-го вопроса. Проверка ответа')
+    def test_questions_7(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
@@ -77,8 +77,8 @@ class TestQuestions:
 
         assert section_questions.text_answer(MainPage.answer_6) == 'Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.'
 
-    @allure.title('Тест 8-го вопроса')
-    def test_text_questions_8(self, driver):
+    @allure.title('Тест 8-го вопроса. Проверка ответа')
+    def test_questions_8(self, driver):
         self.driver = driver
         base_page = BasePage(self.driver)
         section_questions = Questions(self.driver)
